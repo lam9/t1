3aa81cd800cf059623c8b8a5e570c5536d2200c4
lmo <laurent.moine@divtec.ch>
2016-10-24 08:55:23 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lmo
+ * Date: 25.08.16
+ * Time: 10:40
+ */
+
+echo date("H:m:s"),"<br>";
+
+
+$nb=10;
+for($i=0;$i<10;$i++){
+    $nb++;
+    echo $nb, ' ';
+}
+
+
+
+
